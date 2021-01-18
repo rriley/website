b94589e3bd3a42c163146032d3313f3d105c2ad1
Ryan Riley <rriley.cs@gmail.com>
2021-01-18 14:32:47 +0300

    Tweaking push script

--- _scripts/push.sh
+++ _scripts/push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jekyll build && rsync -r --progress --rsync-path="aklog cs.cmu.edu && rsync" _site/* rdriley@unix.andrew.cmu.edu:www-cs/
+bundle exec jekyll build && rsync -r --progress --rsync-path="aklog cs.cmu.edu && rsync" _site/* rdriley@unix.andrew.cmu.edu:www-cs/
