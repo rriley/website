dd6a2706ed7d81689e19e654b5b3b16dbcd3eb67
Ryan Riley <rriley.cs@gmail.com>
2026-07-20 11:41:16 -0600

    Updating push script to use docker

--- _scripts/push.sh
+++ _scripts/push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bundle exec jekyll build && rsync -r --progress --rsync-path="aklog cs.cmu.edu && rsync" _site/* rdriley@unix.andrew.cmu.edu:www-cs/
+docker run --rm -v `pwd`:/site --net='host' -it bretfisher/jekyll build && rsync -r --progress --rsync-path="aklog cs.cmu.edu && rsync" _site/* rdriley@unix.andrew.cmu.edu:www-cs/
